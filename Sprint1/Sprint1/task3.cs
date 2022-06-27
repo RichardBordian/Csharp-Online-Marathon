ca258b136306a83c824d7f77815d281d8e6d3ce6
RichiWild <75180499+RichiWild@users.noreply.github.com>
2022-06-27 12:32:57 +0300

    Update task3.cs

--- Sprint1/Sprint1/task3.cs
+++ Sprint1/Sprint1/task3.cs
@@ -1,4 +1,4 @@
-﻿//Create class Fraction with private int fields numerator and denominator that can only be initialized on creation or in constructor
+//Create class Fraction with private int fields numerator and denominator that can only be initialized on creation or in constructor
 //Create a constructor to initialize these values
 //Define operators unary and binary + and - 
 //Define the ! operator that will return reversed fraction - with numerator as denominator and denominator as numerator
@@ -31,31 +31,46 @@ class Fraction
     {
         int nsk = NSK(a.denominator, b.denominator);
         int c = nsk / a.denominator * a.numerator + nsk / b.denominator * b.numerator;
-        return new Fraction(c, nsk);
+        int nsd = NSD(c, nsk);
+        if (nsd == 0) return new Fraction(c, nsd);
+        return new Fraction(c / nsd, nsk / nsd);
     }
     public static Fraction operator -(Fraction a, Fraction b)
     {
         int nsk = NSK(a.denominator, b.denominator);
         int c = nsk / a.denominator * a.numerator - nsk / b.denominator * b.numerator;
-        return new Fraction(c, nsk);
+        int nsd = NSD(c, nsk);
+        if (nsd == 0) return new Fraction(c, nsd);
+        return new Fraction(c / nsd, nsk / nsd);
     }
     private static int NSD(int m, int n)
     {
+        if (m < 0 || n < 0) return 0;
         while (m != n)
         {
             if (m > n) m -= n;
             if (n > m) n -= m;
         }
         return m;
+
     }
     private static int NSK(int m, int n) => m * n / NSD(m, n);
-    public string ToString(Fraction a)
+    public override string ToString()
     {
-        int c = NSD(a.numerator, a.denominator);
-        int n = a.numerator / c;
-        int d = a.denominator / c;
-
-        return n + "/" + d;
+        int c = NSD(numerator, denominator);
+        int n = numerator / c;
+        int d = denominator / c;
+        if (d < 0 && n < 0)
+        {
+            d = -d;
+            n = -n;
+        }
+        if (d < 0)
+        {
+            d = -d;
+            return " -" + n + " / " + d;
+        }
+        return n + " / " + d;
     }
     public static Fraction simplified(Fraction a) => new Fraction(a.numerator / NSD(a.numerator, a.denominator), a.denominator / NSD(a.numerator, a.denominator));
     public override int GetHashCode()
@@ -66,21 +81,15 @@ class Fraction
     {
         return base.Equals(obj);
     }
-    //    public static bool operator ==(MyAccessModifiers first, MyAccessModifiers second)
-    //    {
-    //        if (first.Name == second.Name && first.Age == second.Age && first.personalInfo == second.personalInfo)
-    //        {
-    //            return true;
-    //        }
-    //        return false;
-    //    }
-    //    public static bool operator !=(MyAccessModifiers first, MyAccessModifiers second)
-    //    {
+    public static bool operator ==(Fraction a, Fraction b)
+    {
+        if (simplified(a) == simplified(b)) return true;
+        return false;
+    }
+    public static bool operator !=(Fraction a, Fraction b)
+    {
 
-    //        if (first.Name == second.Name && first.Age == second.Age && first.personalInfo == second.personalInfo)
-    //        {
-    //            return false;
-    //        }
-    //        return true;
-    //    }
+        if (simplified(a) == simplified(b)) return false;
+        return true;
+    }
 }
